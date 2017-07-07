dbc88d560b025bd32bbcb1fdb86610cbc1c40df1
niveditha <nivibe96@gmail.com>
2017-07-07 14:05:16 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Natural
+{
+  
+  public static void main(String args[])throws IOException
+  {
+    
+ 
+    int a;
+     // BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     Scanner s=new Scanner(System.in);
+          a=s.nextInt();
+          int sum=0;
+          for(int i=1;i<=a;i++)
+          {
+             sum=sum+i;
+             }
+             System.out.println(sum);
+             }
+             }
+            
